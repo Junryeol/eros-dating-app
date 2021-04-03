b975907414f8fcf15018da599d5dc6d1ea3c06e2
SnoopyKim <snoopykim0@gmail.com>
2021-04-03 17:54:02 +0900

    스플래쉬 화면 완료

--- lib/widgets/pages/system/splash_page.dart
+++ lib/widgets/pages/system/splash_page.dart
@@ -1,3 +1,5 @@
+import 'package:eros/widgets/components/scaffolds.dart';
+import 'package:eros/widgets/components/texts.dart';
 import 'package:flutter/material.dart';
 // import 'package:flutter_svg/svg.dart';
 
@@ -9,17 +11,69 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+
+  Widget _buildTitle() {
+    return RichText(
+      textAlign: TextAlign.center,
+      text: TextSpan(
+        children: <TextSpan> [
+          TextSpan(
+            text: "이상형 소개",
+            style: TextStyle(
+              color: Color(0xfff2708f),
+              fontSize: 36.0,
+              fontWeight: FontWeight.w700
+            )
+          ),
+          TextSpan(
+            text: "부터\n",
+            style: TextStyle(
+              color: Color(0xff706569),
+              fontSize: 36.0,
+              fontWeight: FontWeight.w700,
+            )
+          ),
+          TextSpan(
+            text: "오프라인 만남",
+            style: TextStyle(
+              color: Color(0xfff2708f),
+              fontSize: 36.0,
+              fontWeight: FontWeight.w700
+            )
+          ),
+          TextSpan(
+            text: "까지",
+            style: TextStyle(
+              color: Color(0xff706569),
+              fontSize: 36.0,
+              fontWeight: FontWeight.w700
+            )
+          )
+        ]
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffolds.basic(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            SizedBox(height: 12),
-            Text("splash"),
-            // Image.asset("assets/images/logo_text.png"),
-            SizedBox(height: 12),
+            _buildTitle(),
+            SizedBox(height: 96),
+            Image(
+              width: 237,
+              height: 163,
+              image: AssetImage('assets/images/logo.png')
+            ),
+            SizedBox(height: 96),
+            Image(
+              width: 222.7,
+              height: 46,
+              image: AssetImage('assets/images/logo_and_text.png')
+            ),
             // SvgPicture.asset("assets/images/logo.svg"),
           ],
         ),
